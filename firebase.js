7f26935462c0dd96cba9755ae2963b183a649bba
Dede yunus <dedeyunus96@gmail.com>
2024-09-12 00:09:49 +0700

    update

---
+++ firebase.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC2H5tcVHGSLpF08ya8_yJA8JdzV39QLdk",
+    authDomain: "kipas-78879.firebaseapp.com",
+    databaseURL: "https://kipas-78879-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kipas-78879",
+    storageBucket: "kipas-78879.appspot.com",
+    messagingSenderId: "29906188202",
+    appId: "1:29906188202:web:3e0710949345de5f551dd3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
